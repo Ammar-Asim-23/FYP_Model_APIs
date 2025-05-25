553255a7ce2053cee6c4e194a652b0a3c0be821d
Ammar Asim <ammarasim065@gmail.com>
2025-05-25 10:24:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ from model_utils import (
 
 app = FastAPI()
 
-# CORS setup
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://swift-cloud.ephlux.com"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
